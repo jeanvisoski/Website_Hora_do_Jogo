a1e9e901af3efa3c0ed70a2ff1e07e0640a20554
jean.visoski <jean.visoski_est@compasso.com.br>
2018-08-14 09:46:09 -0300

    teste

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,55 +1,52 @@
 import React, { Component } from 'react';
 import './login.css';
+import ReactDOM from 'react-dom';
 
 class Login extends Component {
   render() {
     return (
-      <body class="bg-dark">
-      
-          <div class="container">
-            <div class="card card-login mx-auto mt-5">
-              <div class="card-header">Login</div>
-              <div class="card-body">
-                <form>
-                  <div class="form-group">
-                    <div class="form-label-group">
-                      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="required" autofocus="autofocus"/>
-                      <label for="inputEmail">Email address</label>
-                    </div>
+      <body>
+
+        <div class="container">
+          <div class="card card-login mx-auto mt-5">
+            <div class="card-header">Login</div>
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <div class="form-label-group">
+                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="required" autofocus="autofocus" />
+                    <label for="inputEmail">Email address</label>
                   </div>
-                  <div class="form-group">
-                    <div class="form-label-group">
-                      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="required"/>
-                      <label for="inputPassword">Password</label>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <div class="checkbox">
-                      <label>
-                        <input type="checkbox" value="remember-me"/>
-                        Remember Password
-                      </label>
-                    </div>
-                  </div>
-                  <a class="btn btn-primary btn-block" href="index.html">Login</a>
-                </form>
-                <div class="text-center">
-                  <a class="d-block small mt-3" href="register.html">Register an Account</a>
-                  <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
                 </div>
-              </div>
+                <div class="form-group">
+                  <div class="form-label-group">
+                    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="required" />
+                    <label for="inputPassword">Password</label>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox" value="remember-me" />
+                      Remember Password
+                      </label>
+                  </div>
+                </div>
+                <a class="btn btn-primary btn-block" href="/painelAdmin">Login</a>
+              </form>
             </div>
           </div>
-      
-          
-          <script src="vendor/jquery/jquery.min.js"></script>
-          <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-      
-        
-          <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-      
-        </body>
-            )
+        </div>
+
+
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+      </body>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Website from './components/website/Website';
 import Admin from './components/admin/Admin';
+import Login from './components/login/Login';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+
+
+
+
 ReactDOM.render(
+
     <BrowserRouter>
         <Switch>
             <Route path="/" exact={true} component={Website} />
             <Route path="/admin" component={Admin} />
+            <Route path="/login" component={Login} />
         </Switch>
+        
     </ BrowserRouter>
     , document.getElementById('root'));
 registerServiceWorker();
+
