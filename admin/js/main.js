43d83bc07203f72dd1b424a250bcac439d45217c
jean.visoski <jean.visoski_est@compasso.com.br>
2018-08-17 09:16:28 -0300

    asd

--- admin/js/main.js
+++ admin/js/main.js
@@ -4,6 +4,7 @@ $(function () {
         $('#calendarLink').removeClass('active');
         $('.calendar').addClass('displayNone');
         $('#dashboard').removeClass('displayNone');
+        $('.games').addClass('displayNone');
         $('#dashboard').addClass('displayFlex');
         $('#dashboardLink').addClass('active');
 
@@ -15,8 +16,21 @@ $(function () {
         $('#dashboard').removeClass('displayFlex');
         $('#dashboardLink').removeClass('active');
         $('#dashboard').addClass('displayNone');
+        $('.games').addClass('displayNone');
         $('.calendar').removeClass('displayNone');
         $('#calendarLink').addClass('active');
+
+    });
+});
+
+$(function () {
+    $('#gamesLink').click(function (i) {
+        $('#dashboard').removeClass('displayFlex');
+        $('#dashboardLink').removeClass('active');
+        $('#dashboard').addClass('displayNone');
+        $('.games').removeClass('displayNone');
+        $('#gamesLink').addClass('active');
+        $('.calendar').addClass('displayNone');
 
     });
 });
